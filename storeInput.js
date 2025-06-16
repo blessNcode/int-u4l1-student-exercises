621c348106852fc710a7fc464fa3b200885c0609
blessNcode <blessingoyor2008@gmail.com>
2025-06-16 13:36:25 -0400

    .values

--- storeInput.js
+++ storeInput.js
@@ -1,15 +1,18 @@
 let button = document.querySelector("button");
+let textElem = document.querySelector('.text');
+let numberElem = document.querySelector('.number');
+let emailElem = document.querySelector('.email');
+let dateElem = document.querySelector('.date');
 
 button.addEventListener("click", function() {
-    let text;
-    let number;
-    let email;
-    let date;
+    let text = textElem.value;
+    let number = numberElem.value ;
+    let email = emailElem.value;
+    let date = dateElem.value;
     
     // 1. Update the four variables - text, number, email, and date.
     //  - Save the value of the four different inputs.
     //  - Go to the html to use the class names.
-    
 
     
     
